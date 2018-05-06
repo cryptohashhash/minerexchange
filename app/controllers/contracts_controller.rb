e90840b746c1d6afa1fa32a3ccdea3d500939794
cryptohashhash <lucasnfreddie@gmail.com>
2018-05-06 20:31:14 +1000

    created show view and refined index view

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,6 +1,11 @@
 class ContractsController < ApplicationController
-
+  before_action :find_contract, only: [:show, :edit, :update, :destroy]
   def index
+    @contracts = Contract.all.order("created_at DESC")
+  end 
+
+  def show 
+   
   end 
 
   def new 
@@ -10,6 +15,13 @@ class ContractsController < ApplicationController
 
   def create 
     @contract = Contract.new(contract_params)
+
+    if @contract.save 
+      redirect_to root_path 
+    else 
+      render 'new' 
+    end
+    
   end 
 
 
@@ -18,6 +30,9 @@ class ContractsController < ApplicationController
   def contract_params 
     params.require(:contract).permit(:title, :description, :owner, :price)
   end
-
+  
+  def find_contract
+    @contract = Contract.find(params[:id])
+  end
 
 end
